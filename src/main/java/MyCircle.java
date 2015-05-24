937686da95d37fc3c68843bf2d4d20b726bd0458
Mateusz Cianciara <e.cianciara@gmail.com>
2015-05-25 00:39:09 +0200

    Added comments and new method in MyCircle

--- src/main/java/MyCircle.java
+++ src/main/java/MyCircle.java
@@ -7,10 +7,16 @@ import javafx.scene.shape.Circle;
  */
 public class MyCircle extends Circle {
     final double moveback = 1;
+
+    /*
+    * Contains speed in each axis in "unit" per second
+    * */
     public int vecX;
     public int vecY;
+
     public int mass;
     public int radius;
+
     public MyCircle(double x, double y, int mass, double radius){
         this.setCenterX(x);
         this.setCenterY(y);
@@ -27,14 +33,37 @@ public class MyCircle extends Circle {
         setCenterX(myX + myVecX*sec);
         setCenterY(myY + myVecY * sec);
     }
-    public void changeMovementVector(double x, double y){
 
+    /*
+    * TO DO
+    * */
+    public void changeMovementVector(double x, double y){
     }
+
+    /*
+    * Moves circle from current position by provided values
+    * */
     public void translate(double x, double y){
         setCenterX(getCenterX()+x);
         setCenterY(getCenterY()+y);
     }
+
+    /*
+    * Check if coordinates are inside circle
+    * */
     public boolean inCircle(double x, double y){
+        if( (getCenterX()-x)*(getCenterX()-x)+(getCenterY()-y)*(getCenterY()-y)< getRadius()*getRadius()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
+    * Checks if coordinates are such that creating new circle with center in those coordinates will
+    * create overlapping circles
+    * */
+    public boolean inCollisionProximity(double x, double y){
         if( (getCenterX()-x)*(getCenterX()-x)+(getCenterY()-y)*(getCenterY()-y)< 4*getRadius()*getRadius()){
             return true;
         } else {
