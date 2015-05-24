58f6f102dcf66231dd5e078ccc312035a1a2eeff
Mateusz Cianciara <e.cianciara@gmail.com>
2015-05-25 01:49:13 +0200

    Collisions by Gwinner

--- src/main/java/ExperimentManager.java
+++ src/main/java/ExperimentManager.java
@@ -1,14 +1,10 @@
 
-
 import javafx.scene.control.ScrollBar;
-
 import javafx.scene.shape.Circle;
 import javafx.concurrent.Task;
 import javafx.scene.Group;
-
 import java.lang.Exception;import java.lang.Override;import java.lang.Thread;import java.lang.Void;
 import java.util.Vector;
-
 /**
  * Created by osm on 03/05/15.
  */
@@ -21,32 +17,26 @@ public class ExperimentManager {
     boolean going;
     double eps = 1;
     double howFast;
-
     {
         movingObjects = new Group();
         going = false;
     }
-
     boolean isGoing() {
         return going;
     }
-
     void stop() {
         going = false;
     }
-
     private double abs(double a) {
         if (a < 0.0) {
             return -a;
         }
         return a;
     }
-
     private double length(Circle a, Circle b) {
         return java.lang.Math.sqrt((a.getCenterX() - b.getCenterX()) * (a.getCenterX() - b.getCenterX())
                 + (a.getCenterY() - b.getCenterY()) * (a.getCenterY() - b.getCenterY()));
     }
-
     private boolean tangent(Circle cir1, Circle cir2) {
         double mylen = length(cir1, cir2);
         if (abs(mylen - cir1.getRadius() - cir2.getRadius()) < eps) {
@@ -54,7 +44,6 @@ public class ExperimentManager {
         }
         return false;
     }
-
     void myRun() {
         final Task<Void> task = new Task<Void>() {
             @Override
@@ -63,11 +52,9 @@ public class ExperimentManager {
                     try {
                         Thread.sleep(1000/60);
                     } catch (Exception e) {
-
                     }
                     for (int i = 0; i < figures.size(); i++) {
                         for (int j = i + 1; j < figures.size(); j++) {
-
                             MyCircle cir1 = figures.get(i);
                             MyCircle cir2 = figures.get(j);
                             if (!tangent(cir1, cir2)) {
@@ -76,15 +63,33 @@ public class ExperimentManager {
                             if (abs(cir1.vecX - cir2.vecX) < eps && abs(cir1.vecY - cir2.vecY) < eps) {
                                 continue;
                             }
-                            int newX = cir1.vecX + cir2.vecX;
-                            int newY = cir1.vecY + cir2.vecY;
-                            cir1.vecX += newX;
-                            cir1.vecY += newY;
-                            cir2.vecX += newX;
-                            cir2.vecY += newY;
+
+
+
+
+                            double cs = (double) cir1.getCenterX() - (double) cir2.getCenterX() ;
+                            double ss = (double) cir1.getCenterY() - (double) cir2.getCenterY() ;
+
+
+                            double x1 = cir1.vecX * cs - cir1.vecY * ss;
+                            double y1 = cir1.vecX * ss + cir1.vecY * cs;
+
+                            double x2 = cir2.vecX * cs - cir2.vecY * ss;
+                            double y2 = cir2.vecX * ss + cir2.vecY * cs;
+
+                            x1 = x1 + x2;
+                            x2 = x1;
+
+
+                            cir1.vecX = (int)(x1 * cs - y1 * ss);
+                            cir1.vecX = (int)(y1 * ss + y1 * cs);
+
+                            x2 = x2 * cs - y2 * ss;
+                            y2 = x2 * ss + y2 * cs;
+
+
                         }
                     }
-                    //System.out.println(figures.size());
                     for (MyCircle cir : figures) {
                         double sec = 0.1;
                         sec *= scroll.getValue();
@@ -99,13 +104,10 @@ public class ExperimentManager {
         moveCircles.start();
         myThreads.add((moveCircles));
         going = true;
-
     }
-
     void restart() {
         going = false;
     }
-
     void setScroll(ScrollBar a) {
         scroll = a;
     }
